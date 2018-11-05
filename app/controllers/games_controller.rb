c22a1ebe3f111f030df85ee94ded9a67aea909f9
Kaif Ahmed <hello@kaif.me>
2018-11-05 17:31:22 +0000

    added styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,38 +3,44 @@ require 'json'
 
 class GamesController < ApplicationController
   def new
+    # define an empty array and fill it with 10 random letters
     @grid = [] 
     @grid << ("A".."Z").to_a.sample while @grid.length < 10
   end
 
   def score
-    split_attempt = params["user_answer"].upcase.split("")
+    # split the user's guess and the grid string into arrays so we can check it.
+    @split_attempt = params["user_answer"].upcase.split("")
     split_grid = params["grid"].split("")
-    letter_check = split_attempt.all? { |letter| split_attempt.count(letter) <= split_grid.count(letter) }
+
+    # check if the letter is in the grid
+    letter_check = @split_attempt.all? { |letter| @split_attempt.count(letter) <= split_grid.count(letter) }
     
-    dic_return = load_json(params["user_answer"])
-    dic_check = dic_return["found"]
+    # check if the word is in the dictionary
+    dic_check = dictionary(params["user_answer"])
 
-    @message = game_state(dic_check, letter_check)
+    # generate the end of game message based on these parameters
+    @message = generate_message(dic_check, letter_check, params["user_answer"])
   end
 
-  def load_json(input)
-    file_path = "https://wagon-dictionary.herokuapp.com/#{input}"
+  def dictionary(user_answer)
+    # use the wagon dictionary API to check if word is in the dictionary
+    file_path = "https://wagon-dictionary.herokuapp.com/#{user_answer}"
     json_file = open(file_path).read
-
-    return JSON.parse(json_file)
+    response = JSON.parse(json_file)
+    return response["found"]
   end
 
-  def game_state(dic_check, letter_check)
+  def generate_message(dic_check, letter_check, user_answer)
     if letter_check && dic_check
-      score = params["user_answer"].length * 100
-      @message = "Congratulations, you scored #{score} points!"
+      score = user_answer.length * 100
+      return "Congratulations, the word '#{user_answer.upcase}' scored #{score} points!"
     elsif !dic_check && letter_check
-      @message = "Your word is in the grid, but it's not an English word!"
+      return "'#{user_answer.upcase}' is in the grid, but it's not an English word!"
     elsif dic_check && !letter_check
-      @message = "That's a real word, but it ain't in the grid!"
+      return "'#{user_answer.upcase}' is a real word, but it ain't in the grid!"
     elsif !dic_check && !letter_check
-      @message = "That is neither in the grid or an actual word. Pathetic!"
+      return "'#{user_answer.upcase}' is neither in the grid or an actual word. Pathetic!"
     end
   end
 
